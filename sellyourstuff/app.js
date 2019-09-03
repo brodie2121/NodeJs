843921af3f4b7fe8a6ef52648e9d4a308910f397
Brodie Brock <brodiebrock@gmail.com>
2019-09-03 12:07:39 -0500

    adding home page

--- sellyourstuff/app.js
+++ sellyourstuff/app.js
@@ -1,28 +1,39 @@
-const express = require('express')
-const app = express()
-const mustacheExpress = require('mustache-express')
-const bodyParser = require('body-parser')
-const path = require('path')
-const models = require('./models')
-const bcrypt = require('bcrypt')
-const session = require('express-session')
-const indexRoutes = require('./routes/index')
+const express = require("express");
+const app = express();
+const mustacheExpress = require("mustache-express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const models = require("./models");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const indexRoutes = require("./routes/index");
+const userRoutes = require("./routes/users");
 
-const PORT = 3000
-const VIEWS_PATH = path.join(__dirname,'/views')
+const PORT = 3000;
+const VIEWS_PATH = path.join(__dirname, "/views");
 
-app.use(session({
-  secret: 'somesecret',
-  resave: true,
-  saveUninitialized: false
-}))
+global.__basedir = __dirname;
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(
+  session({
+    secret: "somesecret",
+    resave: true,
+    saveUninitialized: false
+  })
+);
 
-app.engine('mustache',mustacheExpress(VIEWS_PATH + '/partials','.mustache'))
-app.set('views',VIEWS_PATH)
-app.set('view engine','mustache')
+// static folder
+app.use("/uploads", express.static("uploads"));
+// localhost:3000/css/site.css
+app.use("/css", express.static("css"));
 
-app.use('/',indexRoutes)
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(PORT,() => console.log('Server is running...'))
+app.engine("mustache", mustacheExpress(VIEWS_PATH + "/partials", ".mustache"));
+app.set("views", VIEWS_PATH);
+app.set("view engine", "mustache");
+
+app.use("/", indexRoutes);
+app.use("/users", userRoutes);
+
+app.listen(PORT, () => console.log("Server is running..."));
--- sellyourstuff/routes/index.js
+++ sellyourstuff/routes/index.js
@@ -1,93 +1,82 @@
-const express = require('express')
-const router = express.Router()
-const bcrypt = require('bcrypt')
-const models = require('../models')
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const models = require("../models");
 
-const SALT_ROUNDS = 10
+const SALT_ROUNDS = 10;
 
-router.get('/login',(req,res) => {
-  res.render('login')
-})
+router.get("/", async (req, res) => {
+  let products = await models.Product.findAll();
+  res.render("index", { products: products });
+});
 
-router.get('/register',(req,res) => {
-  res.render('register')
-})
+router.get("/login", (req, res) => {
+  res.render("login");
+});
 
-router.post('/register',async (req,res) => {
+router.get("/register", (req, res) => {
+  res.render("register");
+});
 
-  let username = req.body.username
-  let password = req.body.password
+router.post("/register", async (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
 
   let persistedUser = await models.User.findOne({
     where: {
       username: username
     }
-  })
+  });
 
-  if(persistedUser == null) {
-
+  if (persistedUser == null) {
     bcrypt.hash(password, SALT_ROUNDS, async (error, hash) => {
-
-      if(error) {
-        res.render('/register',{message: 'Error creating user!'})
+      if (error) {
+        res.render("/register", { message: "Error creating user!" });
       } else {
-
         let user = models.User.build({
           username: username,
           password: hash
-        })
+        });
 
-        let savedUser = await user.save()
-        if(savedUser != null) {
-          res.redirect('/login')
+        let savedUser = await user.save();
+        if (savedUser != null) {
+          res.redirect("/login");
         } else {
-          res.render('/register',{message: "User already exists!"})
+          res.render("/register", { message: "User already exists!" });
         }
-
       }
-
-    })
-
+    });
   } else {
-      res.render('/register',{message: "User already exists!"})
+    res.render("/register", { message: "User already exists!" });
   }
+});
 
-})
-
-router.post('/login', async (req,res) => {
-
-  let username = req.body.username
-  let password = req.body.password
+router.post("/login", async (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
 
   let user = await models.User.findOne({
     where: {
       username: username
     }
-  })
+  });
 
-  if(user != null) {
-
-    bcrypt.compare(password, user.password,(error, result) => {
-
-      if(result) {
-
+  if (user != null) {
+    bcrypt.compare(password, user.password, (error, result) => {
+      if (result) {
         // create a session
-        if(req.session) {
-          req.session.user = {userId: user.id}
-          res.redirect('/users/products')
+        if (req.session) {
+          req.session.user = { userId: user.id };
+          res.redirect("/users/products");
         }
-
       } else {
-        res.render('login',{message: 'Incorrect username or password'})
+        res.render("login", { message: "Incorrect username or password" });
       }
-
-    })
-
-  } else { // if the user is null
-    res.render('login',{message: 'Incorrect username or password'})
+    });
+  } else {
+    // if the user is null
+    res.render("login", { message: "Incorrect username or password" });
   }
+});
 
-})
-
-
-module.exports = router
+module.exports = router;
